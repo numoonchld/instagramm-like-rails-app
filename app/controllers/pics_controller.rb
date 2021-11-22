50d7f9d76370b67a5bcf2f8e4f2a56e0a81ab16c
numoonchld <raghavendra.saralaya@gmail.com>
2021-11-22 16:35:28 +0530

    Modified view filed to HAML

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,2 +1,21 @@
 class PicsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @pic = Pic.new
+  end
+
+  # does not have a view file, responsible for create logic
+  def create 
+    @pic = Pic.new(pic_params)
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
+
 end
