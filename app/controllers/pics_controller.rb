1ff4dd498756857f045a790816efcab8769ad05e
numoonchld <raghavendra.saralaya@gmail.com>
2021-11-22 17:20:29 +0530

    Edit, Update and Delete actions in view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,7 +2,7 @@ class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
-    @pics = Pic.all
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def show 
@@ -23,6 +23,24 @@ class PicsController < ApplicationController
     end
     
   end
+
+  def edit
+  end
+
+  # does not have a view file, responsible for "updation" logic
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Pic was updated"
+    else 
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
   
   private
   
