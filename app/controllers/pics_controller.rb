8cf02f25a6160645a57c96af9434f12af5799334
numoonchld <raghavendra.saralaya@gmail.com>
2021-11-22 17:02:23 +0530

    Show single pic and index pages

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,21 +1,37 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all
   end
 
+  def show 
+  end
+  
   def new
     @pic = Pic.new
   end
-
-  # does not have a view file, responsible for create logic
+  
+  # does not have a view file, responsible for "creation" logic
   def create 
     @pic = Pic.new(pic_params)
+    
+    if @pic.save
+      redirect_to @pic, notice: "Pic was posted"
+    else 
+      render 'new'
+    end
+    
   end
-
+  
   private
-
+  
   def pic_params
     params.require(:pic).permit(:title, :description)
+  end
+  
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
 
 end
